5c9d10df1407ccd9c3b051d564c8b1842789467b
May W <51608792+May-W@users.noreply.github.com>
2019-07-01 02:00:39 -0500

    Fixed some more bugs

--- main.py
+++ main.py
@@ -43,12 +43,11 @@ def validate_form():
         password2_error = "Passwords must match"
 
     if email != '':
-        if length_valid == False or contains_spaces == True or count_at != 1 or count_dot != 1:
+        if length_valid == False or contains_spaces == True or one_at == False or one_dot == False:
             email_error = "Email must be between 3 and 20 characters, must not contain spaces, and may only contain one at and one dot"
 
-
     if not username_error and not password_error and not password2_error and not email_error:
-        return redirect('/welcome')
+        return redirect('/welcome?name={0}'.format(username))
     else: 
         return render_template('login_form.html', username_error = username_error, password_error = password_error, password2_error = password2_error, email_error = email_error, username=username, password='', password2='', email=email)
 
@@ -56,8 +55,6 @@ def validate_form():
 def is_blank(string):
     if len(string) == 0:
         return True
-    else:
-        return False
 
 def length_valid(string):
     if len(string) > 20 or len(string) < 3:
@@ -75,23 +72,30 @@ def contains_spaces(string):
     else:
         return False
 
-def count_at(string):
+def one_at(string):
     at = 0
     for c in string:
-        if sc == '@':
+        if c == '@':
             at = at + 1
-    return at
+    if at == 1:
+        return True
+    else:
+        return False
 
-def count_dot(string):
+def one_dot(string):
     dot = 0
     for c in string:
-        if c == '@':
+        if c == '.':
             dot = dot + 1
-    return dot    
+    if dot == 1:
+        return True
+    else:
+        return False
 
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html', title="Welcome!")
+    username = request.args.get('name')
+    return render_template('welcome.html', title="Welcome!", username=username)
 
 app.run()
